ff6a679677b0726b3635e02e94abaaa06bcd06ad
Lambord_Evgeny <lambordevg@gmail.com>
2024-02-11 21:49:20 +0200

    Done: Implemented the game module. Fixed a few bugs that were found in the tests. Passed all the tests.

--- Homework_1/game.erl
+++ Homework_1/game.erl
@@ -1,3 +1,24 @@
--module(game)
--export([canWin/1, nextMove/1, explanation/0])
+-module(game).
+-export([canWin/1, nextMove/1, explanation/0]).
 
+
+canWin(0,{P1,P2})-> {P1,P2};
+canWin(MatchesAmount,{P1,P2})-> canWin(MatchesAmount - 1, {(not(P1 and P2)), P1}).
+
+
+%Requested
+%-------------------------------------------------------------------------------------------------------------------
+canWin(MatchesAmount) when is_integer(MatchesAmount) andalso 0 < MatchesAmount -> 
+		element(1, canWin(MatchesAmount - 1, {true, false})).
+%-------------------------------------------------------------------------------------------------------------------
+nextMove(1)	-> {true, 1};
+nextMove(2)	-> {true, 2};
+nextMove(MatchesAmount) when is_integer(MatchesAmount) andalso 0 < MatchesAmount ->
+	{P1, P2} = canWin(MatchesAmount - 2, {true, false}),
+	if
+		not P1	-> {true, 1};
+		not P2	-> {true, 2};
+		true 	-> false
+	end.
+%-------------------------------------------------------------------------------------------------------------------
+explanation() -> {"Similar to Fibonaci Nth term calculation, you have to know who are both the previous values in order to calculate the current"}.
--- Homework_1/shapes.erl
+++ Homework_1/shapes.erl
@@ -1,38 +1,57 @@
--module(shapes)
--export([shapesArea/1, squaresArea/1, trianglesArea/1, shapesFilter/1, shapesFilter2/1])
+-module(shapes).
+-export([shapesArea/1, squaresArea/1, trianglesArea/1, shapesFilter/1, shapesFilter2/1]).
 
 % Area Auxilary Calculation Functions:
-triangleAreaCalc({dim, Base, Height}) when Base > 0, Height > 0 -> 
-		Base * Height / 2.
+isShapeValid({rectangle, {dim, Width, Height}}) when 0 < Width, 0 < Height -> ok;
+	isShapeValid({triangle, {dim, Base, Height}}) when 0 < Base, 0 < Height -> ok;
+	isShapeValid({ellipse, {radius, Rad1, Rad2}}) when 0 < Rad1, 0 < Rad2 -> ok.
 
-squareAreaCalc({dim, Width, Height}) when Width > 0, Height > 0 -> 
-		2 * triangleAreaCalc({dim, Width, Height}).
+triangleAreaCalc({dim, Base, Height}) when 0 < Base, 0 < Height -> 
+		(Base * Height) / 2.
 
-ellipseAreaCalc({ellipse, {radius, Radius1, Radius2}}) when Radius1 > 0, Radius2 > 0 -> 
+rectangleAreaCalc({dim, Width, Height}) when 0 < Width, 0 < Height -> 
+		Width * Height.
+
+ellipseAreaCalc({radius, Radius1, Radius2}) when 0 < Radius1, 0 < Radius2 -> 
 		math:pi() * Radius1 * Radius2.
 
 shapesAreaCalc([], CurrSum) -> CurrSum;
 shapesAreaCalc([ {triangle, Data} | Tail ], CurrSum) -> shapesAreaCalc(Tail, CurrSum + triangleAreaCalc(Data));
-shapesAreaCalc([ {rectangle, Data} | Tail ], CurrSum) -> shapesAreaCalc(Tail, CurrSum + squareAreaCalc(Data));
+shapesAreaCalc([ {rectangle, Data} | Tail ], CurrSum) -> shapesAreaCalc(Tail, CurrSum + rectangleAreaCalc(Data));
 shapesAreaCalc([ {ellipse, Data} | Tail ], CurrSum) -> shapesAreaCalc(Tail, CurrSum + ellipseAreaCalc(Data)).
 
 squaresAreaCalc([], CurrSum) -> CurrSum;
-squaresAreaCalc([ {rectangle, Data} | Tail ], CurrSum) -> squaresAreaCalc(Tail, CurrSum + squareAreaCalc(Data));
-squaresAreaCalc([ _ | Tail ], CurrSum) -> squaresAreaCalc(Tail, CurrSum).
+squaresAreaCalc([ {rectangle, {dim, W, H}} | Tail ], CurrSum) when W =:= H -> 
+		squaresAreaCalc(Tail, CurrSum + rectangleAreaCalc({dim, W, H}));
+squaresAreaCalc([ Shape | Tail ], CurrSum) -> isShapeValid(Shape), squaresAreaCalc(Tail, CurrSum).
 
 trianglesAreaCalc([], CurrSum) -> CurrSum;
-trianglesAreaCalc([ {triangle,, Data} | Tail ], CurrSum) -> squaresAreaCalc(Tail, CurrSum + triangleAreaCalc(Data));
+trianglesAreaCalc([ {triangle, Data} | Tail ], CurrSum) -> trianglesAreaCalc(Tail, CurrSum + triangleAreaCalc(Data));
 trianglesAreaCalc([ _ | Tail ], CurrSum) -> trianglesAreaCalc(Tail, CurrSum).
 
+
 %Requested
+%-------------------------------------------------------------------------------------------------------------------
 shapesArea({shapes, ShapesToCalc}) -> shapesAreaCalc(ShapesToCalc, 0).
 
+%-------------------------------------------------------------------------------------------------------------------
 squaresArea({shapes, ShapesToCalc}) -> squaresAreaCalc(ShapesToCalc, 0).
 
+%-------------------------------------------------------------------------------------------------------------------
 trianglesArea({shapes, ShapesToCalc}) -> trianglesAreaCalc(ShapesToCalc, 0).
 
-shapesFilter(WhichShape) -> fun(X) -> lists:filter(fun({WhichShape, _ }) -> true; (_) -> false end, X).
+%-------------------------------------------------------------------------------------------------------------------
+shapesFilter(WhichShape) -> 
+		fun({shapes, X}) -> {shapes, lists:filter(fun({GivenShape, Data}) -> isShapeValid({GivenShape, Data}), WhichShape =:= GivenShape end, X)} 
+			end.
 
-shapesFilter2(circle) -> fun(X) -> lists:filter(fun({elipse, {radius, Y, Y}}) -> true; (_) -> false end, X);
-shapesFilter2(square) -> fun(X) -> lists:filter(fun({rectangle, {dim, Y, Y}}) -> true; (_) -> false end, X);
+%-------------------------------------------------------------------------------------------------------------------
+shapesFilter2(circle) -> 
+		fun({shapes, X}) -> {shapes, lists:filter(fun({ellipse, {radius, Y, Y}}) -> true; 
+																	({Shape, Data}) -> isShapeValid({Shape, Data}), false end, X)} 
+			end;
+shapesFilter2(square) -> 
+		fun({shapes, X}) -> {shapes, lists:filter(fun({rectangle, {dim, Y, Y}}) -> true; 
+																   ({Shape, Data}) -> isShapeValid({Shape, Data}), false end, X)} 
+			end;
 shapesFilter2(WhichShape) -> shapesFilter(WhichShape).
